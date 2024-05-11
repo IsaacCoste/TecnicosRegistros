6bc6b49ee0a5d7bb54dd65760bdbf2d75c11dde8
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-05-10 21:53:50 -0400

    Elimine los corchetes del namespace

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TecnicosRegistros.Models;
 
-namespace TecnicosRegistros.DAL
+namespace TecnicosRegistros.DAL;
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
-        public DbSet<Tecnicos> Tecnicos { get; set; }
-        public Contexto(DbContextOptions<Contexto> options) : base(options)
-        {
-        }
     }
 }
