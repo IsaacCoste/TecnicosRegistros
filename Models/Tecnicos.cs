f2bfaa3d5f20c3b39ba07319d968f14de15423ee
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-05-10 21:46:47 -0400

    Cree le modelo de Tecnicos

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecnicosRegistros.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras.")]
+        [Required(ErrorMessage = "Es obligatorio introducir el Nombre del técnico.")]
+        public string Nombres { get; set; }
+        [Range(0.01, float.MaxValue, ErrorMessage = "Es obligatorio introducir el SueldoHora")]
+        public decimal SueldoHora { get; set; }
+    }
+}
