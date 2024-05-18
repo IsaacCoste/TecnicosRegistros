90f90775d54d2ffcba855b155083800b0de900d5
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-05-18 10:09:57 -0400

    Cree el modelo de tipos de tecnicos

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TecnicosRegistros.Models;
+public class TiposTecnicos
+{
+    [Key]
+    public int TipoId { get; set; }
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras.")]
+    [Required(ErrorMessage = "Es obligatorio introducir la Descripción del técnico.")]
+    public string Descripción { get; set; }
+}
