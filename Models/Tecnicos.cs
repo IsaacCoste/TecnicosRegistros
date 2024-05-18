bcdb53cb1a0f5649511cabfc4a7afee674a68645
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-05-18 14:12:16 -0400

    Coloque que se puedan agregar las vocales con tildes en el modelo de tecnicos, en  nombres

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -6,7 +6,7 @@ public class Tecnicos
 {
     [Key]
     public int TecnicoId { get; set; }
-    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras.")]
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "Solo se permiten letras y vocales con tilde.")]
     [Required(ErrorMessage = "Es obligatorio introducir el Nombre del técnico.")]
     public string Nombres { get; set; }
     [Range(0.01, float.MaxValue, ErrorMessage = "Es obligatorio introducir el SueldoHora")]
