c8a24aba6514318aa5fa6ff2c744daf29df42809
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-05-26 17:57:53 -0400

    Agrege un nuevo campo en el modelo de TiposTecnicos el campo de Incentivo

--- Models/TiposTecnicos.cs
+++ Models/TiposTecnicos.cs
@@ -7,4 +7,6 @@ public class TiposTecnicos
     [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "Solo se permiten letras y vocales con tilde.")]
     [Required(ErrorMessage = "Es obligatorio introducir la Descripcion del técnico.")]
     public string Descripcion { get; set; }
+    public decimal? Incentivo { get; set; }
+    public Tecnicos? Tecnicos { get; set; }
 }
